08c9df4c5cfbf4a5ac15e74046cc75113230f352
Kenneth Osei-Tutu Marfo <kobbie377@gmail.com>
2024-01-04 13:22:11 -0800

    Hard again

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
-!/usr/bin/python3
 from add_0 import add
 
-a,b = 1,2
+a = 1
+b = 2
 
-print("{} + {} = {}".format(a, b, )add(a, b))
+result = add(a, b)
+
+print("{} + {} = {}".format(a, b, result))
+
