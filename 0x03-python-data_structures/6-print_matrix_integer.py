dfdccbf26439a05942098c36cad4c1ce81716142
Kenneth Osei-Tutu Marfo <kobbie377@gmail.com>
2024-01-08 07:30:44 -0800

    Hard

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def print_matrix_integer(matrixi=[]):
+def print_matrix_integer(matrixi=[[]]):
     for i in matrixi:
         for j in i:
             print("{}".format(j), end=" ")
