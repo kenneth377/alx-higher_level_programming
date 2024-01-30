f92ed35d72d0c01c26ccc3582aee685f8178f198
Kenneth Osei-Tutu Marfo <kobbie377@gmail.com>
2024-01-30 07:17:31 -0800

    Hard

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def magic_string():
     magic_string.iteration = getattr(magic_string, "iteration",0) +1
     return ("BestSchool, " * (magic_string.iteration - 1) + "BestSchool")
