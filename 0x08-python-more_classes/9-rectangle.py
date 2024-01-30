b5a3b8bdabeb1257cc6a31960d42efd92e5e47ad
Kenneth Osei-Tutu Marfo <kobbie377@gmail.com>
2024-01-29 16:28:18 -0800

    Hard

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -8,6 +8,8 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
     def __init__(self, width=0, height=0,):
+        if width < 0  or height<0:
+            raise ValueError("Width and Height must be greater than zero")
         self.__height = height
         self.__width = width
         Rectangle.number_of_instances+=1
