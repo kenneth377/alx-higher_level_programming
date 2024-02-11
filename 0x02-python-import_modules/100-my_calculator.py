628630ffd076cbff76075bed517c063287618e7e
Kenneth Osei-Tutu Marfo <kobbie377@gmail.com>
2024-02-11 14:06:18 -0800

    Hard

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,9 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    if len(sys.argv) - 1 != 3:
+    
+    cc = len(sys.argv) - 1
+    if cc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
